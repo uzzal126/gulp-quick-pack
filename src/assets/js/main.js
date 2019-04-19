a65c913e9fcc2526d940464a0db583322f0cc389
Uzzal hossain <uzzal@hastechit.com>
2019-04-20 02:05:41 +0600

    Gulp Quick Pack

---
+++ src/assets/js/main.js
@@ -0,0 +1,19 @@
+(function ($) {
+	"use strict";
+
+	// Scroll to top active js
+	$(window).on('scroll', function () {
+		if ($(this).scrollTop() > 600) {
+			$('.scroll-top').removeClass('not-visible');
+		} else {
+			$('.scroll-top').addClass('not-visible');
+		}
+	});
+	$('.scroll-top').on('click', function (event) {
+		$('html,body').animate({
+			scrollTop: 0
+		}, 1000);
+	});
+	
+})(jQuery);
+
